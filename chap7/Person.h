bc482bb163831918fccc085c958bda8b31da4b8e
kaka@313Ubuntu1410 <eechengy@gmail.com>
2014-11-20 11:38:25 +0800

    Up to exercise 7.2.1, Page 243

--- chap7/Person.h
+++ chap7/Person.h
@@ -2,20 +2,23 @@
 #define PERSON_H
 
 struct Person {
-    Person() = default;
-    Person(const std::string &sname, const std::string &sadd) : name(sname), address(sadd) {}
-    Person(std::istream &is) {
-        is >> name >> address;
-    }
-    std::string getName() const {
-        return name;
-    }
-    std::string getAddress() const {
-        return address;
-    }
-
-    std::string name;
-    std::string address;
+    friend std::istream &read(std::istream &is, Person &p);
+    friend std::ostream &print(std::ostream &os, const Person &p);
+    public:
+        Person() = default;
+        Person(const std::string &sname, const std::string &sadd) : name(sname), address(sadd) {}
+        Person(std::istream &is) {
+            is >> name >> address;
+        }
+        std::string getName() const {
+            return name;
+        }
+        std::string getAddress() const {
+            return address;
+        }
+    private:
+        std::string name;
+        std::string address;
 };
 
 std::istream &read(std::istream &is, Person &p);
--- chap7/Sales_data.h
+++ chap7/Sales_data.h
@@ -1,24 +1,30 @@
 #ifndef SALES_DATA_H
 #define SALES_DATA_H
 
-struct Sales_data {
-    Sales_data() = default;
-    Sales_data(const std::string &s) : bookNo(s) {}
-    Sales_data(const std::string &s, unsigned n, double p):
-        bookNo(s), units_sold(n), revenue(p*n) {}
-    Sales_data(std::istream &is) {
-        double price;
-        is >> bookNo >> units_sold >> price;
-        revenue = price * units_sold;
-    }
+class Sales_data {
+    friend Sales_data add(const Sales_data&, const Sales_data&);
+    friend std::ostream &print(std::ostream&, const Sales_data&);
+    friend std::istream &read(std::istream&, Sales_data&);
 
-    std::string isbn() const { return bookNo; }
-    Sales_data& combine(const Sales_data&);
-    double avg_price() const;
+    public:
+        Sales_data() = default;
+        Sales_data(const std::string &s) : bookNo(s) {}
+        Sales_data(const std::string &s, unsigned n, double p):
+            bookNo(s), units_sold(n), revenue(p*n) {}
+        Sales_data(std::istream &is) {
+            double price;
+            is >> bookNo >> units_sold >> price;
+            revenue = price * units_sold;
+        }
 
-    std::string bookNo;
-    unsigned units_sold = 0;
-    double revenue = 0.0;
+        std::string isbn() const { return bookNo; }
+        Sales_data& combine(const Sales_data&);
+
+    private:
+        double avg_price() const;
+        std::string bookNo;
+        unsigned units_sold = 0;
+        double revenue = 0.0;
 };
 
 Sales_data add(const Sales_data&, const Sales_data&);
